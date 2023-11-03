aba2da75d60a8af62d29521789063b6b7d4dd5e9
pochiman <pochiman@yahoo.com>
2023-11-02 22:39:21 -0400

    initial commit

---
+++ App-001-Browser-Automation-Web-Scraping/main_8_login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import chromedriver_autoinstaller
+
+def get_driver():
+    # Set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                          # and if it doesn't exist, download it automatically,
+                                          # then add chromedriver to path
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://automated.pythonanywhere.com/login/")
+    return driver
+
+def main():
+    driver = get_driver()
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+    print(driver.current_url)
+
+print(main())
