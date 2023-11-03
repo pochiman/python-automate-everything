50cf0fa8b21a39c0a841e89fae518a4c0161205a
pochiman <pochiman@yahoo.com>
2023-11-03 12:04:40 -0400

    initial commit

---
+++ App-002-Accessing-Building-APIs/main_26_grammar_correction_API.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = 'https://api.languagetool.org/v2/check'
+data = {
+    'text': 'Tis is a nixe day!',
+    'language':'auto'
+}
+
+response = requests.post(url, data=data)
+result = json.loads(response.text)
+print(result)
