d40e4cf00058ddc3e422401bea9737cce79a9b15
pochiman <pochiman@yahoo.com>
2023-11-03 20:24:39 -0400

    initial commit

---
+++ App-003-Files-Folders/main_30_rename_files_based_on_sub-sub-folders.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+root_dir = Path('files')
+
+for path in root_dir.glob("**/*"):
+    if path.is_file():
+        parent_folder = path.parts
+        subfolders = path.parts[1:-1]
+
+        new_filename = "-".join(subfolders) + '-' + path.name
+        print(new_filename)
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
