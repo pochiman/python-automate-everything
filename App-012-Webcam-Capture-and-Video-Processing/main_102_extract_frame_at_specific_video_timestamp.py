eabcfd327434eda5a731f0f8ff20fc567946b60f
pochiman <pochiman@yahoo.com>
2023-11-06 08:49:25 -0500

    initial commit

---
+++ App-012-Webcam-Capture-and-Video-Processing/main_102_extract_frame_at_specific_video_timestamp.py
@@ -0,0 +1,19 @@
+import cv2
+
+video = cv2.VideoCapture("video.mp4")
+
+nr_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+fps = video.get(cv2.CAP_PROP_FPS)
+
+timestamp = input('Enter timestamp in hh:mm:ss format: ')
+
+timestamp_list = timestamp.split(':')
+hh, mm, ss = timestamp_list
+timestamp_list_floats = [float(i) for i in timestamp_list]
+hours, minutes, seconds = timestamp_list_floats
+
+frame_nr = hours * 3600 * fps + minutes * 60 * fps + seconds * fps
+
+video.set(1, frame_nr)
+success, frame = video.read()
+cv2.imwrite(f'Frame at {hh}:{mm}:{ss}.jpg', frame)
